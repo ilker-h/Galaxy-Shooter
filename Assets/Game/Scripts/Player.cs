e02965b815efd1e80a0331f832a272bc99d3386a
ilker-h <ilker.hadzhalaran@gmail.com>
2019-12-27 20:18:47 -0500

    Upload repo from BitBucket

---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public bool canTripleShot = false;
+    public bool hasSpeedBoost = false;
+    public bool shieldsActive = false;
+    public int lives = 3;
+
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private GameObject _tripleShotPrefab;
+    [SerializeField]
+    private GameObject _explosionPrefab;
+    [SerializeField]
+    private GameObject _shieldGameObject;
+    [SerializeField]
+    private float _fireRate = 0.25f;
+    private float _canFire = 0.0f;
+    [SerializeField]
+    private float _speed = 5.0f;
+
+    private UIManager _uiManager;
+    private GameManager _gameManager;
+    private SpawnManager _spawnManager;
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // current position = new position
+        transform.position = new Vector3(0, 0, 0);
+
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+
+        if (_uiManager != null)
+        {
+            _uiManager.UpdateLives(lives);
+        }
+
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        if (_spawnManager != null)
+        {
+            _spawnManager.StartSpawnRoutines();
+        }
+        
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        };
+    }
+
+    private void Shoot()
+    {
+        if (Time.time > _canFire)
+        {
+            _audioSource.Play();
+            if (canTripleShot == true)
+            {
+                Instantiate(_tripleShotPrefab, transform.position, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+            }
+
+            _canFire = Time.time + _fireRate;
+        };
+    }
+
+    private void Movement()
+    {
+        // movement of player
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (hasSpeedBoost == true)
+        {
+            transform.Translate(Vector3.right * _speed * 1.5f * horizontalInput * Time.deltaTime);
+            transform.Translate(Vector3.up * _speed * 1.5f * verticalInput * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.right * _speed * horizontalInput * Time.deltaTime);
+            transform.Translate(Vector3.up * _speed * verticalInput * Time.deltaTime);
+        }
+
+        // constricting feature for vertical
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        };
+
+        // wrapping feature for horizontal
+        if (transform.position.x > 8.903436f)
+        {
+            transform.position = new Vector3(-8.903436f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -8.903436f)
+        {
+            transform.position = new Vector3(8.903436f, transform.position.y, 0);
+        };
+
+    }
+
+    public void Damage()
+    {
+        if (shieldsActive == true)
+        {
+            shieldsActive = false;
+            _shieldGameObject.SetActive(false);
+            return;
+        }
+
+        lives = lives - 1;
+        _uiManager.UpdateLives(lives);
+
+        if (lives < 1)
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            _gameManager.gameOver = true;
+            _uiManager.ShowTitleScreen();
+            Destroy(this.gameObject);
+        }
+    }
+
+    // coroutines
+    public IEnumerator TripleShotPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        canTripleShot = false;
+    }
+
+    public IEnumerator SpeedBoostPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        hasSpeedBoost = false;
+    }
+
+    public void TripleShotPowerupOn()
+    {
+        canTripleShot = true;
+        StartCoroutine(TripleShotPowerDownRoutine());
+    }
+
+    public void SpeedBoostPowerupOn()
+    {
+        hasSpeedBoost = true;
+        StartCoroutine(SpeedBoostPowerDownRoutine());
+    }
+
+    public void EnableShields()
+    {
+        shieldsActive = true;
+        _shieldGameObject.SetActive(true);
+    }
+    
+}
+
+
---
+++ Assets/Game/Scripts/SpawnManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyShipPrefab;
+    [SerializeField]
+    private GameObject[] powerups;
+    private GameManager _gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+        StartCoroutine(EnemySpawnRoutine());
+        StartCoroutine(PowerupSpawnRoutine());
+    }
+
+    public void StartSpawnRoutines()
+    {
+        StartCoroutine(EnemySpawnRoutine());
+        StartCoroutine(PowerupSpawnRoutine());
+    }
+
+    IEnumerator EnemySpawnRoutine()
+    {
+        while (_gameManager.gameOver == false)
+        {
+            Instantiate(enemyShipPrefab, new Vector3(Random.Range(-7f, 7f), 7, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    IEnumerator PowerupSpawnRoutine()
+    {
+        while (_gameManager.gameOver == false)
+        {
+            int randomPowerup = Random.Range(0, 3);
+            Instantiate(powerups[randomPowerup], new Vector3(Random.Range(-7f, 7f), 7, 0), Quaternion.identity);
+            yield return new WaitForSeconds(10.0f);
+        }
+    }
+}
